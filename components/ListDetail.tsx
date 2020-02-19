a72f7d2cac6d8019d7db0162aaf5935df0239de3
antnzhlnkn <antnzhlnkn@gmail.com>
2020-02-19 14:38:25 +0300

    delete styles

--- components/ListDetail.tsx
+++ components/ListDetail.tsx
@@ -26,37 +26,7 @@ const ListDetail: React.FunctionComponent<ListDetailProps> = ({
             .content {
                 background: #E5E5E5;
                 font-family: Exo, sans-serif;
-            }
-            .container__center{
-                max-width: 1600px;
-                margin: auto;
-                padding: 10px;
-                flex-wrap: wrap;
-                display: flex;
-            }
-           .content__title {
-                display: flex;
-                font-family: Exo, sans-serif;
-                font-style: normal;
-                font-weight: 600;
-                font-size: 36px;
-                line-height: 48px;
-                text-transform: uppercase;
-                color: #333333;
-                margin-top: 32px;
-                margin-bottom: 36px;
-            }
-            .content__title_border {
-                padding-left: 10px;
-                margin-left: 0;
-            }
-            
-           .content__title_border::before {
-                padding: 10px;
-                content: '';
-                border-left: 3px solid #000000;
-            }
-                      
+            }     
             .beer-about {
                 display: flex;
                 flex-wrap: wrap;
